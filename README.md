3520f34b283bdf0dee40b3d9c94a4d9b1a002792
sashimimochi <104ki.clarkpgmail.com>
2024-11-19 17:57:40 +0900

    ローマ字対応

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Solr Tutorial
 
-全文検索エンジン Solr を手元で試す用のサンプルコード。
-環境は Docker で構築する想定になっている。
+全文検索エンジンSolrを手元で試す用のサンプルコード。
+環境はDockerで構築する想定になっている。
 
 | Service   | Version |
 | :-------- | :------ |
-| SolrCloud | 9.0.0   |
+| SolrCloud | 9.4.0   |
 | Zookeeper | 3.6     |
-| MySQL     | 5.7     |
+| MariaDB   | 10.5    |
 
 # Quick Start
 
@@ -15,13 +15,13 @@
 $ sh ./launch.sh
 ```
 
-正常に data-import まで環境していれば、検索できるようになる。
+正常にdata-importまで環境していれば、検索できるようになる。
 
 ```bash
 $ curl "http://localhost:8080/solr/ldgourmet/select?indent=true&indent=true&q.op=OR&q=*:*"
 ```
 
-Solr の管理画面には以下でアクセスできる。
+Solrの管理画面には以下でアクセスできる。
 http://localhost:8080/solr/
 
 いらなくなったデータの削除方法は以下の通り。
@@ -32,7 +32,7 @@ $ make clean
 
 # Maintenance
 
-Solr の実態は `opt/solr/bin/solr` にある。
+Solrの実態は `opt/solr/bin/solr` にある。
 コンテナのホームディレクトリは `opt/solr` なので、`bin/solr`でたどり着ける。
 
 ```bash
--- launch.sh
+++ launch.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
-if [ ! -d "mysql/data/ldgourmet/" ]; then
+collection="ldgourmet"
+
+if [ ! -d "mysql/data/${collection}/" ]; then
     make csv
 fi
 
@@ -13,14 +15,21 @@ done
 
 # Install DataImportHandler
 REPO_URL="https://raw.githubusercontent.com/SearchScale/dataimporthandler/master/repo"
-docker-compose exec solr_node1 bin/solr package add-repo data-import-handler ${REPO_URL}
-docker-compose exec solr_node1 bin/solr package list-available
-docker-compose exec solr_node1 bin/solr package install data-import-handler
-docker-compose exec solr_node1 server/scripts/cloud-scripts/zkcli.sh -zkhost zookeeper1:2181 -cmd upconfig -confdir /opt/solr/server/solr/configsets/ldgourmet/conf -confname ldgourmet_conf
-curl "http://localhost:8080/solr/admin/collections?action=CREATE&name=ldgourmet&collection.configName=ldgourmet_conf&numShards=1&replicationFactor=1&maxShardsPerNode=1"
-docker-compose exec solr_node1 bin/solr package deploy data-import-handler -y -collections ldgourmet
-curl "localhost:8080/solr/admin/collections?action=RELOAD&name=ldgourmet"
+
+if [ `docker-compose exec solr_node1 bin/solr package list-installed | grep data-import-handler | wc -l` -eq 0 ]; then
+    docker-compose exec solr_node1 bin/solr package add-repo data-import-handler ${REPO_URL}
+    docker-compose exec solr_node1 bin/solr package list-available
+    docker-compose exec solr_node1 bin/solr package install data-import-handler
+fi
+docker-compose exec solr_node1 server/scripts/cloud-scripts/zkcli.sh -zkhost zookeeper1:2181 -cmd upconfig -confdir /opt/solr/server/solr/configsets/${collection}/conf -confname ${collection}_conf
+
+if [ `curl http://localhost:8080/solr/admin/collections?action=LIST | grep ${collection} | wc -l` -eq 1 ]; then
+    curl "http://localhost:8080/solr/admin/collections?action=DELETE&name=${collection}"
+fi
+curl "http://localhost:8080/solr/admin/collections?action=CREATE&name=${collection}&collection.configName=${collection}_conf&numShards=1&replicationFactor=1&maxShardsPerNode=1"
+docker-compose exec solr_node1 bin/solr package deploy data-import-handler -y -collections ${collection}
+curl "localhost:8080/solr/admin/collections?action=RELOAD&name=${collection}"
 
 # Full-Import data
 sleep 30
-curl "http://localhost:8080/solr/ldgourmet/dataimport?command=full-import&entity=tutorial"
+curl "http://localhost:8080/solr/${collection}/dataimport?command=full-import&entity=tutorial"
